39d18f43ad94e7b55debd89ec8754fca410253b5
18eddymedina <18emedina@gmail.com>
2019-01-19 18:31:58 -0600

    e

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
